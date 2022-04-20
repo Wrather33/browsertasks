a419ba234742f8259865ca337ef6fc048fa75110
Wrather33 <dlyad0nata43@gmail.com>
2022-04-20 14:42:53 +0300

    add

---
+++ GetGifs/getgif.js
@@ -0,0 +1,36 @@
+(function get(){
+    let sub = document.getElementById('submit')
+    let input = document.getElementById('get')
+    let img = document.querySelector('#gif > img')
+    let getfile = document.getElementById('getfile')
+    sub.onclick = function(){
+    let tag = input.value
+    input.value = ''
+    fetch(`https://api.giphy.com/v1/gifs/random?api_key=R0GPJboG4KOQcUEAvtwSmgSy7Le1joZ9&tag=${tag}&rating=g`).then(r=> {return r.json()})
+    .then(d=> 
+        img.src = d.data.images.original.url
+    ).catch(e=>alert(e))
+    }
+    getfile.onchange = function(){
+       let type = getfile.files[0].type
+       if(type === 'image/gif'){
+           let gettag = prompt('Tags?')
+           let file = getfile.files[0]
+           if(gettag){
+               fetch("https://upload.giphy.com/v1/gifs", {
+               method: "POST",
+               body: new URLSearchParams({
+                   api_key: "R0GPJboG4KOQcUEAvtwSmgSy7Le1joZ9",
+                   source_image_url: file,
+                   tags: gettag
+                })    
+            }).then((response) => {
+                alert(`status: ${response}`);
+            }).catch(e => alert(e));
+       }}
+       else{
+           alert('Wrong type!')
+       }
+    }
+
+})()
